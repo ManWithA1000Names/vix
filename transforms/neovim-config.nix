e2539a192267eeb25e00b811f65e58767fb19eef
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-08-03 23:14:19 +0300

    add everything?

--- transforms/neovim-config.nix
+++ transforms/neovim-config.nix
@@ -1,1 +1,61 @@
-{ pkgs, name, lua }: _: "touch $out/${name}/init.lua"
+{ pkgs, name, lua }:
+{ init ? ""
+, set ? { }
+, globals ? { }
+, colorscheme ? ""
+, keybinds ? { }
+, enable_vim_loader ? true
+}:
+let
+  initFile = pkgs.writeText "init.lua" ''
+    ${pkgs.lib.strings.optionalString enable_vim_loader "vim.loader.enable()"}
+    ${lua.toValidLuaInsert init}
+    require("${name}-generated-config.globals")
+    require("${name}-generated-config.set")
+  '';
+
+  setFile = pkgs.writeText "set.lua" ''
+    ${liblua.toValidLuaInsert (pkgs.lib.attrsets.atrrByPath ["lua"] "" set)}
+    ${liblua.toValidLuaInsert (pkgs.lib.foldlAttrs (acc: name: value: acc + "vim.opt.${name} = ${liblua.toLua value};") "" (builtins.removeAttrs set ["lua"]))}
+  '';
+
+  globalsFile = pkgs.writeText "globals.lua" ''
+    ${liblua.toValidLuaInsert (if builtins.hasAttr "lua" globals then globals.lua else "")}
+    ${liblua.toValidLuaInsert (pkgs.lib.foldlAttrs (acc: name: value: acc + "vim.g.${name} = ${liblua.toLua value};") "" (builtins.removeAttrs globals ["lua"]))}
+  '';
+
+  keybindindsFile = pkgs.writeText "${name}-generated-keybindinds.lua" ''
+    --{{ injected lua code
+    ${liblua.toValidLuaInsert (pkgs.lib.attrsets.atrrByPath ["lua"] "" keybinds)}
+    --}}
+
+    -- KEYBINDINDS
+    local whichkey = require("which-key");
+    --{{ normal mode
+    ${pkgs.lib.strings.optionalString (builtins.hasAttr "normal" keybinds) ''whichkey.register(${liblua.toLua keybinds.normal}, {mode = "n"});''}
+    --}}
+
+    --{{ insert mode
+    ${pkgs.lib.strings.optionalString (builtins.hasAttr "insert" keybinds) ''whichkey.register(${liblua.toLua keybinds.insert}, {mode = "i"});''}
+    --}}
+
+    --{{ visual/select mode
+    ${pkgs.lib.strings.optionalString (builtins.hasAttr "visual" keybinds) ''whichkey.register(${liblua.toLua keybinds.visual}, {mode = "v"});''}
+    --}}
+
+    --{{ command mode
+    ${pkgs.lib.strings.optionalString (builtins.hasAttr "command" keybinds) ''whichkey.register(${liblua.toLua keybinds.command}, {mode = "c"});''}
+    --}}
+
+    --{{ terminal mode
+    ${pkgs.lib.strings.optionalString (builtins.hasAttr "terminal" keybinds) ''whichkey.register(${liblua.toLua keybinds.terminal}, {mode = "t"});''}
+    --}}
+  '';
+in
+''
+  cp ${initFile} $out/${name}/init.lua
+  mkdir -p $out/${name}/lua/${name}-generated-config/
+  cp ${setFile} $out/${name}/lua/${name}-generated-config/set.lua
+  cp ${globalsFile} $out/${name}/lua/${name}-generated-config/globals.lua
+  cp ${keybindindsFile} $out/${name}/after/plugin/keybinds.lua
+''
--- transforms/plugins.nix
+++ transforms/plugins.nix
@@ -1,1 +1,22 @@
-{ pkgs, name, lua }: _: "echo plugins"
+{ pkgs, name, lua }:
+
+let
+  compile_plugin_source = { src, dir ? "start" }: "cp ${src} $out/${name}/pack/${name}-plugins/${dir}/";
+
+  compile_plugin_config = app_name: { name ? "", src, configure ? true, urgent ? false, opt ? false, setupFN ? "setup", config ? (lua.defaultPluginConfig { inherit name setupFN; config = { }; }) }:
+    if !configure then compile_plugin_source { inherit src; dir = if opt then "opt" else "start"; }
+    else
+    let
+      config_file = pkgs.writeText "${name}-config" (if builtins.isAttrs config then lua.defaultPluginConfig { inherit name setupFN config; } else config);
+    in
+    ''
+      cp ${config_file} $out/${app_name}/${if urgent then "plugin" else "after/plugin"}/;
+      ${compile_plugin_source {inherit src; dir = if opt then "opt" else "start";}}
+    ''
+  ;
+
+  compiled_plugin = plugin:
+    if builtins.hasAttr "outPath" plugin then compile_plugin_source { src = plugin; }
+    else compile_plugin_config name plugin;
+in
+pkgs.lib.strings.concatMapStrings compiled_plugin
