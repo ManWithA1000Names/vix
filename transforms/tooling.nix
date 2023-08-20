8880186745b29149e35fbf9c2af47d92c0dbe69c
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-08-20 14:25:56 +0300

    ..

--- transforms/tooling.nix
+++ transforms/tooling.nix
@@ -54,7 +54,7 @@ let
       tools;
     in assert they_are_valid; tools;
 
-  tools = builtins.abort String.toString merged_tools_set/*validate (Dict.values merged_tools_set)*/;
+  tools = builtins.abort (String.toString merged_tools_set)/*validate (Dict.values merged_tools_set)*/;
   language-servers = builtins.abort (String.toString tools); #List.filter (item: builtins.abort (String.toString item) /*item.type == "language-server"*/) tools;
   null-ls-tools = builtins.abort (String.toString tools) List.filter (item: item.type != "language-server") tools;
 
