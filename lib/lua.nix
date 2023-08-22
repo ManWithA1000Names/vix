7fb9ec517d2d50514a5e9349b3096c7d33421a78
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-08-22 18:06:09 +0300

    fix(keybinds): forget to resolve the final cmd from the value provided

--- lib/lua.nix
+++ lib/lua.nix
@@ -1,4 +1,31 @@
-{ nilm, pkgs }: rec {
+{ nilm, pkgs }:
+let
+  resolveCmd = key: value:
+    if nilm.Nix.isA "tuple" value then
+      nilm.String.toString (nilm.Tuple.first value)
+    else if nilm.Nix.isA "list" value then
+      if nilm.List.length value 0 then
+        builtins.abort "Found keymapping to an list of length 0!"
+      else
+        nilm.String.toString (nilm.List.get 0 value)
+    else if nilm.Nix.isA "string" value then
+      nilm.String.toString value
+    else if nilm.Nix.isA "lambda" value then
+      let res = value null; in assert nilm.Nix.isA "string" res; res
+    else
+      builtins.abort
+      "Found keymapping (${key}) to a value that is not either a: string, list, tuple or lambda.";
+
+  removeRecursively = toremove:
+    nilm.Dict.foldl (key: value: acc:
+      if key == toremove then
+        acc
+      else if nilm.Nix.isA "set" value then
+        nilm.Dict.insert key (removeRecursively toremove value) acc
+      else
+        nilm.Dict.inset key value acc) { };
+
+in rec {
   toLuaTableKey = arg:
     if nilm.String.startsWith "[" arg && nilm.String.endsWith "]" arg then
       arg
@@ -8,6 +35,14 @@
   toLua = arg:
     if nilm.Nix.isA "string" arg then
       nilm.String.concat [ "[[" arg "]]" ]
+    else if nilm.Nix.isA "tuple" arg then
+      nilm.String.concat [
+        "{"
+        (toLua (nilm.Tuple.first arg))
+        ","
+        (toLua (nilm.Tuple.second arg))
+        "}"
+      ]
     else if nilm.Nix.isA "list" arg then
       nilm.String.concat [
         "{"
@@ -53,6 +88,8 @@
   toKeybindings = mode: bindings:
     toValidLuaInsert (nilm.Dict.foldl (key: value: acc:
       acc + ''
-        vim.keymap.set("${mode}", "${key}", "${value}", {noremap = true, silent = true});'')
-      "" (nilm.Dict.flatten bindings));
+        vim.keymap.set("${mode}", "${key}", ${
+          resolveCmd key value
+        }, {noremap = true, silent = true});'') ""
+      (nilm.Dict.flatten (removeRecursively "name" bindings)));
 }
