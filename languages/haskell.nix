e98b2661ec122f1dd1c6cee99843e6072083c6fa
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-09-01 13:54:36 +0300

    update haskell

--- languages/haskell.nix
+++ languages/haskell.nix
@@ -2,7 +2,7 @@ pkgs: [
   {
     # TODO: weird haskell trying to find the language-server with the same version as the GHC on the PATH
     type = "language-server";
-    pkg = pkgs.haskell-language-server;
+    pkg = pkgs.haskellPackages.haskell-language-server;
     name = "hls";
     exe = "haskell-language-server-wrapper";
   }
