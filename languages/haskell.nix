e2096825aed23a02fa566bab5830e6da5f09ddbb
ManWithA1000Names <loena.phivos2010@gmail.com>
2023-09-01 14:08:15 +0300

    fix typo

--- languages/haskell.nix
+++ languages/haskell.nix
@@ -3,7 +3,7 @@ pkgs: [
     # TODO: weird haskell trying to find the language-server with the same version as the GHC on the PATH
     type = "language-server";
     pkg = pkgs.writeScriptBin "haskell-language-server" ''
-      export PATH = "${pkgs.ghc}/bin:$PATH"
+      export PATH="${pkgs.ghc}/bin:$PATH"
       exec ${pkgs.haskellPackages.haskell-language-server}/bin/haskell-language-server "$@";
     '';
     name = "hls";
