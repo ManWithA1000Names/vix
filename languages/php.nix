c0ae21a464d43bedef950d5b491282a2a5cc20e3
ManWithA1000Names <loena.phivos2010@gmail.com>
2024-09-27 01:52:38 +0300

    remove phpcs/bf for pre-specified tools since phpactor integerates with project local ones

--- languages/php.nix
+++ languages/php.nix
@@ -1,21 +1,4 @@
-pkgs:
-let
-  phpcs = pkgs.writeScriptBin "phpcs" ''
-    if [ -e vendor/bin/phpcs ]; then
-      exec ./vendor/bin/phpcs "$@"
-    else
-      exec ${pkgs.php83Packages.php-codesniffer}/bin/phpcs "$@"
-    fi
-  '';
-
-  phpcbf = pkgs.writeScriptBin "phpcbf" ''
-    if [ -e vendor/bin/phpcbf ]; then
-      exec ./vendor/bin/phpcbf "$@"
-    else
-      exec ${pkgs.php83Packages.php-codesniffer}/bin/phpcbf "$@"
-    fi
-  '';
-in [
+pkgs: [
   {
     type = "language-server";
     pkg = pkgs.phpactor;
@@ -23,17 +6,7 @@ in [
   }
   {
     type = "diagnostics";
-    pkg = phpcs;
-    exe = "phpcs";
-  }
-  {
-    type = "diagnostics";
     pkg = pkgs.php;
     exe = "php";
-  }
-  {
-    type = "formatting";
-    pkg = phpcbf;
-    exe = "phpcbf";
   }
 ]
